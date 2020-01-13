0099a13624b4721e4f9c11c106b7d5bf71eb1499
Lucas Wanderley <lucas.wanderley@virtus.ufcg.edu.br>
2020-01-13 17:22:10 -0300

    finish first module

---
+++ simplemooc/simplemooc/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', 'simplemooc.core.views.home', name='home'),
+    url(r'^contato/$', 'simplemooc.core.views.contact', name='contact'),
+    url(r'^admin/', include(admin.site.urls)),
+)
